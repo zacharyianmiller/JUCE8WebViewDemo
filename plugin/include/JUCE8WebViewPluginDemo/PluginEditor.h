861328e27c06564039985f11a4631c9badb6f783
Zachary Miller <zacharyianmiller1@gmail.com>
2025-07-11 14:38:10 -0500

    HTML loader

--- plugin/include/JUCE8WebViewPluginDemo/PluginEditor.h
+++ plugin/include/JUCE8WebViewPluginDemo/PluginEditor.h
@@ -17,6 +17,9 @@ public:
     void resized() override;
 
 private:
+    using Resource = juce::WebBrowserComponent::Resource;
+    std::optional<Resource> getResource(const juce::String& url);
+
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     AudioPluginAudioProcessor& processorRef;
@@ -25,3 +28,43 @@ private:
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
 };
+
+namespace JUCEWebViewUtils 
+{
+
+static auto streamToVector (juce::InputStream& stream)
+{
+    std::vector<std::byte> result ((size_t) stream.getTotalLength());
+    stream.setPosition (0);
+    [[maybe_unused]] const auto bytesRead = stream.read (result.data(), result.size());
+    jassert (bytesRead == (ssize_t) result.size());
+    return result;
+}
+
+static const char* getMimeForExtension (const juce::String& extension)
+{
+    static const std::unordered_map<juce::String, const char*> mimeMap =
+    {
+        { { "htm"   },  "text/html"                },
+        { { "html"  },  "text/html"                },
+        { { "txt"   },  "text/plain"               },
+        { { "jpg"   },  "image/jpeg"               },
+        { { "jpeg"  },  "image/jpeg"               },
+        { { "svg"   },  "image/svg+xml"            },
+        { { "ico"   },  "image/vnd.microsoft.icon" },
+        { { "json"  },  "application/json"         },
+        { { "png"   },  "image/png"                },
+        { { "css"   },  "text/css"                 },
+        { { "map"   },  "application/json"         },
+        { { "js"    },  "text/javascript"          },
+        { { "woff2" },  "font/woff2"               }
+    };
+
+    if (const auto it = mimeMap.find (extension.toLowerCase()); it != mimeMap.end())
+        return it->second;
+
+    jassertfalse;
+    return "";
+}
+
+} // namespace JUCEWebViewUtils. 
--- plugin/src/PluginEditor.cpp
+++ plugin/src/PluginEditor.cpp
@@ -4,20 +4,25 @@
 //==============================================================================
 AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor& p)
     : AudioProcessorEditor (&p), 
-    processorRef (p)
-#ifdef WIN32
-    ,webView(juce::WebBrowserComponent::Options{}
+    processorRef (p),
+    webView(juce::WebBrowserComponent::Options{}
+//  #ifdef MAC
         .withBackend(juce::WebBrowserComponent::Options::Backend::webview2)
-        .withWinWebView2Options(juce::WebBrowserComponent::Options::WinWebView2{}
+        .withWinWebView2Options(juce::WebBrowserComponent::Options::WinWebView2{}    
+        // Define user data folder to bypass potential permission flags
         .withUserDataFolder(juce::File::getSpecialLocation(juce::File::tempDirectory)))
-    )
-#endif
+        .withResourceProvider([this](const auto& url) { return getResource(url); }))
+// #endif
 {
     juce::ignoreUnused (processorRef);
 
     addAndMakeVisible(webView);
     
-    webView.goToURL("https://juce.com");
+    // Ask backend for URL
+    // webView.goToURL("https://juce.com");
+
+    // Ask backend for a resource
+    webView.goToURL(webView.getResourceProviderRoot());
 
     setResizable(true, true);
     setSize (800, 600);
@@ -30,4 +35,22 @@ AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
 void AudioPluginAudioProcessorEditor::resized()
 {   
     webView.setBounds(getLocalBounds());
+}
+
+auto AudioPluginAudioProcessorEditor::getResource(const juce::String& url) -> std::optional<Resource>
+{
+    // Use raw string on WIN32 so backslashes are not read as escape
+    static const auto resourceFileRoot = juce::File{R"(/Users/zacharymiller/STL/dsp/jucedemos/JUCE8WebViewDemo/plugin/ui/public/)"};
+
+    const auto resourceToRetrieve = url == "/" ? "index.html" : url.fromFirstOccurrenceOf("/", false, false);
+
+    const auto resource = resourceFileRoot.getChildFile(resourceToRetrieve).createInputStream();
+
+    if (resource)
+    {
+        const auto extension = resourceToRetrieve.fromLastOccurrenceOf(".", false, false);
+        return Resource { JUCEWebViewUtils::streamToVector(*resource), JUCEWebViewUtils::getMimeForExtension(extension) };
+    }
+
+    return std::nullopt; // std::optional default (e.g., resource is not there)
 }
